f4bdf052668dcb6d503e4eb213890c4f98a592f8
tiberio_s_ferreira <tiberio.ferreiracs@gmail.com>
2025-11-08 08:01:49 -0300

    feat(users): Adiciona a coluna name á tabela users.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_11_05_213102) do
+ActiveRecord::Schema[8.0].define(version: 2025_11_08_105907) do
   create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -30,6 +30,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_11_05_213102) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "admin", default: false
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
